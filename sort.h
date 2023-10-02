a8df0f7c84301ed89d46f4e8eeac8373cdf28589
Mabende <belindamabende@gmail.com>
2023-10-02 09:40:15 -0700

    updated

--- sort.h
+++ sort.h
@@ -35,6 +35,13 @@ int lomuto_partition(int *array, int lowest, int highest, size_t size);
 void swap_nodes(listint_t **list, listint_t *node1, listint_t *node2);
 void cocktail_sort_list(listint_t **list);
 void merge_sort(int *array, size_t size);
+void swap_ints(int *a, int *b);
+int hoare_partition(int *array, size_t size, int left, int right);
+void hoare_sort(int *array, size_t size, int left, int right);
+int get_max(int *array, int size);
+void radix_counting_sort(int *array, size_t size, int sig, int *buff);
+void radix_sort(int *array, size_t size);
+void quick_sort_hoare(int *array, size_t size);
 void merge(int *array, int *left, size_t left_size,
 		int *right, size_t right_size);
 
